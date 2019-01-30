086bbd4fdffca6983f41c022119319e6d6233d86
Andrey Kuznetsov <and.rey.q@yandex.ru>
2019-01-30 14:16:44 +0300

    add pug structure

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ var gulp = require('gulp'),
     pug = require('gulp-pug');
 
 gulp.task('html', function() {
-    return gulp.src('./src/html/*.pug')
+    return gulp.src('./src/html/pages/*.pug')
         .pipe(plumber({
             errorHandler: notify.onError()
         }))
