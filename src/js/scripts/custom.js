fabfeff8b9d64c9bfcf4549c02d2da8c7b3ca8c2
kuznetsovandrey76 <and.rey.q@yandex.ru>
2019-02-07 12:18:52 +0300

    add bootstrap4

--- src/js/scripts/custom.js
+++ src/js/scripts/custom.js
@@ -1,3 +1,19 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+    // ES6
+    class User {
+        constructor(username, email, password) {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+        }
+
+        register() {
+            console.log(`${this.username} is now registered`);
+        }
+    }
+
+    let andrey = new User('Andrey', 'andrey@ya.ru', '123');
+
+    andrey.register();
 });
